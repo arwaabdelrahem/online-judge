0ee9ed3409f46d96e4d19cd08524c377faf3dfec
arwaabdelrahem <arwaabdelrahem22@gmail.com>
2023-04-19 00:35:52 +0200

    feat: :sparkles: validate many inputs

--- src/problems/problems.service.ts
+++ src/problems/problems.service.ts
@@ -5,6 +5,7 @@ import { DockerSandBox } from './docker-sandbox';
 import { CreateProblemDto } from './dtos/create-problem.dto';
 import { SolveProblemDto } from './dtos/solve-problem.dto';
 import { ProblemsRepo } from './repos/problems.repo';
+import { Problem } from './schemas/problems.schema';
 
 @Injectable()
 export class ProblemsService {
@@ -23,87 +24,99 @@ export class ProblemsService {
     const { languageId, code } = body;
 
     const problem = await this._problemsRepo.findById(problemId);
+
+    const response = await this.validateUserCode(problem, languageId, code);
+
+    return response;
+  }
+
+  private async validateUserCode(
+    problem: Problem,
+    languageId: string,
+    code: string,
+  ) {
     const passedTestCases = [];
 
-    const testCases = problem.testCases.map(async (testCase) => {
-      const stdin_data = testCase.input;
+    const testCases = await Promise.all(
+      problem.testCases.map(async (testCase) => {
+        const stdin_data = testCase.input;
 
-      if (!problem) {
-        throw new NotFoundException('problem not found');
-      }
+        if (!problem) {
+          throw new NotFoundException('problem not found');
+        }
 
-      const language = await this._languagesService.findById(languageId);
+        const language = await this._languagesService.findById(languageId);
 
-      // const path = __dirname + '/'; //current working path
-      const folder = 'temp/' + this.random(10); //folder in which the temporary folder will be saved
-      const vm_name = 'virtual_machine'; //name of virtual machine that we want to execute
-      const timeout_value = 60; //Timeout Value, In Seconds
+        // const path = __dirname + '/'; //current working path
+        const folder = 'temp/' + this.random(10); //folder in which the temporary folder will be saved
+        const vm_name = 'virtual_machine'; //name of virtual machine that we want to execute
+        const timeout_value = 60; //Timeout Value, In Seconds
 
-      const sandbox = new DockerSandBox({
-        timeout_value,
-        path: '/home/arwa/Documents/my-projects/online-judge/src/problems/',
-        folder,
-        vm_name,
-        compiler_name: language.compilerName,
-        file_name: language.fileName,
-        code,
-        output_command: language.outputCommand,
-        languageName: language.languageName,
-        e_arguments: language.extraArguments,
-        stdin_data,
-      });
-
-      const { data, time, errors } = await sandbox.run();
-
-      const output = data
-        .trim()
-        .split('\n')
-        .map(function (string) {
-          return string.trim();
+        const sandbox = new DockerSandBox({
+          timeout_value,
+          path: '/home/arwa/Documents/my-projects/online-judge/src/problems/',
+          folder,
+          vm_name,
+          compiler_name: language.compilerName,
+          file_name: language.fileName,
+          code,
+          output_command: language.outputCommand,
+          languageName: language.languageName,
+          e_arguments: language.extraArguments,
+          stdin_data,
         });
-      console.log(
-        '🚀 ~ file: problems.service.ts:68 ~ ProblemsService ~ solve ~ output:',
-        output,
-      );
-      const expectedOutput = testCase.expectedOutput
-        .trim()
-        .split('\n')
-        .map(function (string) {
-          return string.trim();
-        });
-      console.log(
-        '🚀 ~ file: problems.service.ts:76 ~ ProblemsService ~ solve ~ expectedOutput:',
-        expectedOutput,
-      );
 
-      if (!expectedOutput.every((val, index) => val === output[index])) {
-        passedTestCases.push(false);
-      } else {
-        passedTestCases.push(true);
-      }
+        const { data, time, errors } = await sandbox.run();
 
-      return {
-        input: testCase.input,
-        output: data,
-        expectedOutput: testCase.expectedOutput,
-        code: code,
-        languageId,
-        errors,
-        time,
-      };
-    });
+        const output = data
+          .trim()
+          .split('\n')
+          .map(function (string) {
+            return string.trim();
+          });
 
-    await Promise.all(testCases);
+        const expectedOutput = testCase.expectedOutput
+          .trim()
+          .split('\n')
+          .map(function (string) {
+            return string.trim();
+          });
 
-    console.log(
-      '🚀 ~ file: problems.service.ts:96 ~ ProblemsService ~ passedTestCases ~ passedTestCases:',
-      passedTestCases,
+        const input = testCase.input
+          .trim()
+          .split('\n')
+          .map(function (string) {
+            return string.trim();
+          });
+
+        if (!expectedOutput.every((val, index) => val === output[index])) {
+          passedTestCases.push(false);
+        } else {
+          passedTestCases.push(true);
+        }
+
+        return {
+          input,
+          output,
+          expectedOutput,
+          // code,
+          languageId,
+          errors,
+          time,
+        };
+      }),
     );
+
     if (passedTestCases.some((val) => val === false)) {
-      console.log('test case failed');
+      return {
+        message: 'wrong answer: test case failed',
+        testCases,
+      };
     } else {
-      console.log('all passed');
-      return testCases[0];
+      return {
+        message: 'Success',
+        testCases,
+      };
     }
   }
 
